54a0afd583dd0818af5db18f640bdbaeef3d8a17
Owner <owner@Owners-MacBook-Pro.local>
2020-04-25 12:28:59 -0400

    Created ResultActivity

--- app/src/main/java/com/example/appprogrammingproject/MainActivity.java
+++ app/src/main/java/com/example/appprogrammingproject/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.appprogrammingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
---
+++ app/src/main/java/com/example/appprogrammingproject/ResultActivity.java
@@ -0,0 +1,33 @@
+package com.example.appprogrammingproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView Result;
+    Button TryAgain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+
+        TryAgain = (Button) findViewById(R.id.button3);
+
+        TryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}

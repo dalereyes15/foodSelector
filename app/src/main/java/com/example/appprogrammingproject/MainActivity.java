6b8cd68f37ed11775d21bfaaf245e5d92f333f05
Owner <owner@Owners-MacBook-Pro.local>
2020-04-25 12:31:32 -0400

    Added clickListener in Go button

--- app/src/main/java/com/example/appprogrammingproject/MainActivity.java
+++ app/src/main/java/com/example/appprogrammingproject/MainActivity.java
@@ -8,12 +8,23 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    Button Go;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Go = (Button) findViewById(R.id.button);
 
+        Go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

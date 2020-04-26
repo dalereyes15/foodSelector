b8f1d06040a6afd7ad18aeab1cf29b2359721621
Dale Reyes <dalereyes4@gmail.com>
2020-04-25 22:55:43 -0400

    getting auto complete support fragment set up

--- app/src/main/java/com/example/appprogrammingproject/MainActivity.java
+++ app/src/main/java/com/example/appprogrammingproject/MainActivity.java
@@ -4,10 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
 
     Button Go;
 
@@ -26,5 +40,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        /**
+         * Places API additions
+         */
+
+        final String api_key = "AIzaSyBsXLFj2Fyy2ceJIQh_sVG20PpCH7aU5dI";
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), api_key, Locale.US);
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+
+            }
+        });
+
     }
 }

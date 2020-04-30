e74914e046b5176bb87eaa5bc831c7c8a35ae514
Owner <owner@Owners-MacBook-Pro.local>
2020-04-29 23:46:51 -0400

    Added Preference Activity

--- app/src/main/java/com/example/appprogrammingproject/PreferencesActivity.java
+++ app/src/main/java/com/example/appprogrammingproject/PreferencesActivity.java
@@ -1,5 +1,20 @@
 package com.example.appprogrammingproject;
 
-public class PreferencesActivity {
+import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    TextView setting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+
+        setting = (TextView) findViewById(R.id.setting);
+
+    }
 }
